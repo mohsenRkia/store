062143723fe2a6781b7b954f6e28e179277fc932
MohsenRkia <mohsenrostamikia@gmail.com>
2018-12-23 20:26:37 +0330

    Create Models And Controllers

---
+++ app/Http/Controllers/SatisfiedcostumerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Satisfiedcostumer;
+use Illuminate\Http\Request;
+
+class SatisfiedcostumerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Satisfiedcostumer  $satisfiedcostumer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Satisfiedcostumer $satisfiedcostumer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Satisfiedcostumer  $satisfiedcostumer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Satisfiedcostumer $satisfiedcostumer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Satisfiedcostumer  $satisfiedcostumer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Satisfiedcostumer $satisfiedcostumer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Satisfiedcostumer  $satisfiedcostumer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Satisfiedcostumer $satisfiedcostumer)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/SpecialofferController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Specialoffer;
+use Illuminate\Http\Request;
+
+class SpecialofferController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Specialoffer  $specialoffer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Specialoffer $specialoffer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Specialoffer  $specialoffer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Specialoffer $specialoffer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Specialoffer  $specialoffer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Specialoffer $specialoffer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Specialoffer  $specialoffer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Specialoffer $specialoffer)
+    {
+        //
+    }
+}
